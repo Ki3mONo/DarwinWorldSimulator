b6fdba039e00afad853b60d0b1421d52e9515674
memecat5 <tomaszglabek55@gmail.com>
2024-12-27 22:22:18 +0100

    Further work on animal and genom to squash too

--- PO_Projekt/src/main/java/agh/isc/oop/project/model/Animal.java
+++ PO_Projekt/src/main/java/agh/isc/oop/project/model/Animal.java
@@ -1,6 +1,7 @@
 package agh.isc.oop.project.model;
 
 import java.util.List;
+import java.util.Random;
 
 public class Animal implements WorldElement{
     private static int initialEnergy;
@@ -11,6 +12,8 @@ public class Animal implements WorldElement{
 
     private static int eatingEnergy;
 
+    protected static int moveEnergy;
+
     public static void setInitialEnergy(int initialEnergy) {
         Animal.initialEnergy = initialEnergy;
     }
@@ -19,28 +22,43 @@ public class Animal implements WorldElement{
         Animal.requiredBreedingEnergy = requiredBreedingEnergy;
     }
 
-    public static void setLostBreedingEnergy(int lostBreedingEnergy) {}
+    public static void setLostBreedingEnergy(int lostBreedingEnergy) {
+        Animal.lostBreedingEnergy = lostBreedingEnergy;
+    }
 
     public static void setEatingEnergy(int eatingEnergy) {
         Animal.eatingEnergy = eatingEnergy;
     }
 
+    public static void setMoveEnergy(int moveEnergy) {
+        Animal.moveEnergy = moveEnergy;
+    }
 
     private Vector2d position;
     private MapDirection orientation;
 
-    private int energy;
+    protected int energy;
 
-    private Genome genome;
+    private final Genome genome;
 
     //Liczba dni życia zwierzaka
-    private int age;
+    protected int age = 0;
 
     //Do generowania początkowych zwierząt
     public Animal(Vector2d position, List<Integer> geneList) {
         this.position = position;
+        this.orientation = MapDirection.getRandomDirection();
         this.genome = new Genome(geneList);
+        this.energy = initialEnergy;
+    }
 
+    //Do tworzenia dzieci
+    public Animal(Animal parent1, Animal parent2) {
+        this.position = new Vector2d(parent1.position.getX(), parent1.position.getY());
+        this.orientation = MapDirection.getRandomDirection();
+        this.genome = new Genome(parent1, parent2);
+        //Energia stracona przez rodziców trafia do dziecka
+        this.energy = 2 * lostBreedingEnergy;
     }
 
     public int getEnergy() {
@@ -56,5 +74,23 @@ public class Animal implements WorldElement{
         return position;
     }
 
+    public void move(){
+        // Obrót zwierzaka zgodnie z aktywnym genem
+        for (int i = 0; i < genome.getActiveGene(); i++) {
+            orientation = orientation.next();
+        }
+        //Aktywacja kolejnego genu
+        genome.updateCurrentGeneIndex();
+
+
+        //Do dopisania, bo muszę wiedzieć, jak będzie wyglądała mapa.
+        //Wydaje mi się, że dobre będzie zrobienie tutaj całej logiki ruchu,
+        //obliczania gdzie pójdzie, czy może itd., i zwrócenie nowej pozycji do mapy
+        energy -= moveEnergy;
+    }
+
+    public void eat(){
+        energy += eatingEnergy;
+    }
 
 }
--- PO_Projekt/src/main/java/agh/isc/oop/project/model/Genome.java
+++ PO_Projekt/src/main/java/agh/isc/oop/project/model/Genome.java
@@ -1,28 +1,48 @@
 package agh.isc.oop.project.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import java.util.Random;
+import java.util.stream.IntStream;
 
 // Klasa reprezentuje genom zwierzaka
 public class Genome {
     private static int length;
+
+    private static int minMutations;
+
+    private static int maxMutations;
+
     public static void setLength(int length) {
         Genome.length = length;
     }
 
+    public static void setMinMutations(int minMutations) {
+        Genome.minMutations = minMutations;
+    }
+
+    public static void setMaxMutations(int maxMutations) {
+        Genome.maxMutations = maxMutations;
+    }
+
     private final List<Integer> genome;
 
-    private int currentGene;
+    private int currentGeneIndex;
 
     //Do genów zwierząt generowanych na początku
     public Genome(List<Integer> genome) {
+        Random rand = new Random();
+        this.currentGeneIndex = rand.nextInt(0, length);
         this.genome = genome;
     }
 
     //Do genów urodzonych zwierząt
     public Genome(Animal parent1, Animal parent2) {
+        Random rand = new Random();
+        this.currentGeneIndex = rand.nextInt(0, length);
+
         Animal strongerParent;
         Animal weakerParent;
 
@@ -45,7 +65,6 @@ public class Genome {
         final List<Integer> weakerParentGenome = weakerParent.getGenome().genome;
         final List<Integer> strongerParentGenome = strongerParent.getGenome().genome;
 
-        Random rand = new Random();
         boolean weakerFromLeft = rand.nextBoolean();
 
         if (weakerFromLeft) {
@@ -64,11 +83,33 @@ public class Genome {
             }
         }
 
-        // Tutaj mutacje
-
-        this.genome = temp;
+        this.genome = mutate(temp, rand.nextInt(minMutations, maxMutations + 1));;
     }
 
+    //Mutacje genomu
+    private List<Integer> mutate(List<Integer> genome, int mutationsNumber) {
+        //Lista liczb od zera do length-1,
+        List<Integer> indexes = new ArrayList<>(IntStream.rangeClosed(0, length - 1).boxed().toList());
 
+        //Losowanie indeksów do zmiany (muszą być unikatowe, więc najlepiej przez shuffle)
+        Collections.shuffle(indexes);
+
+        Random rand = new Random();
+        for (int i = 0; i < mutationsNumber; i++) {
+            genome.set(indexes.get(i), rand.nextInt(8));
+        }
+
+        return genome;
+    }
+
+    //Wartość aktywnego genu
+    public int getActiveGene(){
+        return genome.get(currentGeneIndex);
+    }
+
+    //Zmiany genów po kolei
+    public void updateCurrentGeneIndex(){
+        currentGeneIndex = (currentGeneIndex + 1) % length;
+    }
 
 }
